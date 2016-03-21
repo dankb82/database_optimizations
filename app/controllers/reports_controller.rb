11b2a8d35852b9067acf2cc9a1a2eb38258b84b3
Daniel Buompane <danielbuompane@gmail.com>
2016-03-21 17:17:24 -0400

    Add has_many_through associations

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,22 +3,8 @@ class ReportsController < ApplicationController
 
   def all_data
     @start_time = Time.now
-
-    @sequences = []
-    @genes = []
-    @hits = []
     @assembly = Assembly.find_by_name(params[:name])
-    @assembly.sequences.each do |s|
-      @sequences << s
-      s.genes.each do |g|
-        @genes << g
-        g.hits.each do |h|
-          @hits << h
-        end
-      end
-    end
-    @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
-
+    @hits = @assembly.hits.order(percent_similarity: :desc)
     @memory_used = memory_in_mb
   end
 
