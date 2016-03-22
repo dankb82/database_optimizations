8f00413e0c7f32101255fa9e6fded9f398da68e5
Daniel Buompane <danielbuompane@gmail.com>
2016-03-21 22:02:48 -0400

    Run analysis of runtime

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,6 +8,23 @@ class ReportsController < ApplicationController
     @memory_used = memory_in_mb
   end
 
+
+  def search
+    @start_time = Time.now
+
+    if params[:search_term]
+      @hits = []
+      @hits += Hit.joins(:gene).where('genes.dna like ?', "%#{params[:search_term]}%")
+      @assembly = Assembly.find_by_name(params[:search_term])
+      @hits += @assembly.hits if @assembly
+      @hits += Hit.where('match_gene_name like ?', "%#{params[:search_term]}%" )
+    else
+      @hits = Hit.all.order('percent_similarity DESC')
+    end
+
+    @memory_used = memory_in_mb
+  end
+
   private def memory_in_mb
     `ps -o rss -p #{$$}`.strip.split.last.to_i / 1024
   end
